ba351a808cc110f45389a8abedfdebf0da9b5006
fredi16 <fredigjoni70@gmail.com>
2022-01-25 23:47:09 +0100

    fixed b and c points

--- index.js
+++ index.js
@@ -26,50 +26,9 @@ const activate = (option) => {
     }
 }
 
-// point A
-const networkClass = (octet) => {
-    if(octet>=0 && octet<=127) return 'A'
-    if(octet>=128 && octet<=191) return 'B'
-    if(oct>=192 && oct<=223) return 'C'
-    return null;
+const getBinaryIp = (ip) => {
+    return `${binaryConvertor(ip[0], 8)}${binaryConvertor(ip[1], 8)}${binaryConvertor(ip[2], 8)}${binaryConvertor(ip[3], 8)}`;
 }
-
-function findNetwork(nClass, octet1, octet2, octet3){
-    if(nClass === 'A') return `${octet1}.0.0.0`
-    if(nClass === 'B') return `${octet1}.${octet2}.0.0`
-    if(nClass === 'C') return `${octet1}.${octet2}.${octet3}.0`
-    return null
-}    
-
-const sameNetwork = (ip1, ip2) => {
-    const network1 = findNetwork(networkClass(parseInt(ip1[0])), ip1[0], ip1[1], ip1[2]);
-    const network2 = findNetwork(networkClass(parseInt(ip2[0])), ip2[0], ip2[1], ip2[2]);
-    console.log(network1, network2);
-    if(network1 == null || network2 == null){
-        alert(`IPs not valid`);
-    }
-    else if(network1 == network2){
-        document.querySelector('.result-a').textContent = `Same network: Yes`;
-    }else{
-        document.querySelector('.result-a').textContent = `Same network: No`;
-    }
-}
-
-const validateA = (e) => {
-    e.preventDefault();
-    const ip1 = formA.querySelector('#ip1').value;
-    const ip2 = formA.querySelector('#ip2').value;
-    if(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip1) && /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip2)){
-        sameNetwork(ip1.split('.'), ip2.split('.'));
-    }else{
-        alert(`IPs not valid`);
-    }
-}
-
-calculateA.addEventListener('click', validateA);
-// end point A
-
-// point B
 
 const stringReverse = (str) => {
     if (str === ""){
@@ -105,11 +64,11 @@ const convertToDecimal = (number) =>{
 function eDecimal (str, c, num){
     let newString = "";
     for (let index=0; index < str.length; index=index+num){
-        var sbstr = str.substr(index,num);
+        var sbstr = str.slice(index, index+num);
         var dec = convertToDecimal(sbstr);
         newString+=dec+c;
     }
-    newString = newString.substr(0, newString.length-1);
+    newString = newString.slice(0, newString.length-1);
     return newString;
 }
 
@@ -122,9 +81,56 @@ const fill = (word, c, length) => {
     return str;
 }
 
+
+// point A
+
+const sameNetwork = (ip1, ip1sub, ip2, ip2sub) => {
+    let network1 = getBinaryIp(ip1);
+    let network2 = getBinaryIp(ip2);
+    network1 = network1.slice(0, ip1sub);
+    network2 = network2.slice(0, ip2sub);
+    const res1 = fill(network1, '0', 32);
+    const res2 = fill(network2, '0', 32);
+    if(res1 == res2){
+        document.querySelector('.result-a').textContent = `Same network: Yes`;
+    }else{
+        document.querySelector('.result-a').textContent = `Same network: No`;
+    }
+}
+
+const validateA = (e) => {
+    e.preventDefault();
+    let ip1 = formA.querySelector('#ip1').value;
+    let ip2 = formA.querySelector('#ip2').value;
+    if(!ip1.includes("/") && !ip2.includes("/")){
+        alert("IPs not valid");
+        return
+    }
+    ip1 = ip1.split('/');
+    ip2 = ip2.split('/');
+    if(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip1[0]) && 
+        /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip2[0]) && 
+        ip1[1] &&
+        ip2[1] &&
+        (ip1[1] >= 0 && ip1[1] <= 32) &&
+        (ip2[1] >= 0 && ip2[1] <= 32))
+    {
+        sameNetwork(ip1[0].split('.'), ip1[1], ip2[0].split('.'), ip2[1]);
+    }else{
+        alert(`IPs not valid`);
+    }
+}
+
+calculateA.addEventListener('click', validateA);
+// end point A
+
+// point B
+
+
+
 const generateNetworkIdnBroadcastId = (ip1, sub) => {
-    let ip = `${binaryConvertor(ip1[0],8)}${binaryConvertor(ip1[1],8)}${binaryConvertor(ip1[2],8)}${binaryConvertor(ip1[3],8)}`;
-    let res = ip.substr(0, sub);
+    let ip = getBinaryIp(ip1);
+    let res = ip.slice(0, sub);
     let networkId = fill(res, '0', 32);
     networkId = eDecimal(networkId,'.', 8);
     let broadcastId = fill(res,'1',32);
@@ -135,15 +141,16 @@ const generateNetworkIdnBroadcastId = (ip1, sub) => {
 
 const validateB = (e) => {
     e.preventDefault();
-    if(!formB.querySelector('#ip1').value.includes("/")){
-        alert("Subnet mask not valid");
+    let ip1 = formB.querySelector('#ip1').value;
+    if(!ip1.includes("/")){
+        alert("IP not valid");
         return
     }
-    const ip1 = formB.querySelector('#ip1').value.split('/');
-    if(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip1[0]) && (ip1[1] >= 0 && ip1[1] <= 32)){
+    ip1 = ip1.split('/');
+    if(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip1[0]) && ip1[1] && (ip1[1] >= 0 && ip1[1] <= 32)){
         generateNetworkIdnBroadcastId(ip1[0].split("."), ip1[1]);
     }else{
-        alert(`IPs not valid`);
+        alert(`IP not valid`);
     }
 }
 
@@ -151,30 +158,49 @@ calculateB.addEventListener('click', validateB);
 // end point B
 
 // point C
-const generateSubnetIdnBroadcastId = (ip1, sub) => {
-    let ip = `${binaryConvertor(ip1[0],8)}${binaryConvertor(ip1[1],8)}${binaryConvertor(ip1[2],8)}${binaryConvertor(ip1[3],8)}`;
-    let res = ip.substr(0, sub);
-    let broadcastId = fill(res,'1',32);
-    broadcastId = eDecimal(broadcastId,'.', 8);
-    let subnetId = "";
-    for (var i=1; i<=sub; i++){
-        subnetId+="1";
+
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
+
+const generateSubnetIdnBroadcastId = (networkId, numberOfSubs) => {
+    let ip = getBinaryIp(networkId.split("."));
+    let mask = ip.lastIndexOf("1");
+    let numOfBits = numberOfSubs%2 === 0 ? 0 : 1;
+    for(let i = 2; i<=numberOfSubs; i *= 2){
+        numOfBits += 1;
     }
-    subnetId = fill(subnetId,'0',32);
-    subnetId = eDecimal(subnetId,'.', 8);
-    document.querySelector('.result1-b').textContent = `Subnet ID: ${subnetId}`;
-    document.querySelector('.result2-b').textContent = `Broadcast ID: ${broadcastId}`;
+    let cidr = "";
+    for(let i = 0; i<ip.length;i++){
+        if(i<=mask){
+            cidr += "1";
+        }else{
+            cidr += "0";
+        }
+    }
+    const subnetMask = eDecimal(cidr,'.', 8);
+    document.querySelector('.result1-c').textContent = `Subnet ID: ${subnetMask}`;
+    console.log(ip);
+    let broadcastBinary = "";
+    for(let i = 0; i<ip.length;i++){
+        if(i>=mask+numOfBits){
+            broadcastBinary += "1";
+        }else{
+            broadcastBinary += ip.charAt(i);
+        }
+    }
+    const broadcastId = eDecimal(broadcastBinary,'.', 8);
+    document.querySelector('.result2-c').textContent = `Broadcast ID: ${broadcastId}`;
+
 }
 
 const validateC = (e) => {
     e.preventDefault();
-    if(!formB.querySelector('#ip1').value.includes("/")){
-        alert("Subnet mask not valid");
-        return
-    }
-    const ip1 = formB.querySelector('#ip1').value.split('/');
-    if(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(ip1[0]) && (ip1[1] >= 0 && ip1[1] <= 32)){
-        generateSubnetIdnBroadcastId(ip1[0].split("."), ip1[1]);
+    const networkId = formC.querySelector('#ip1').value;
+    const numberOfSubs = formC.querySelector('#numberOfSubs').value;
+    if(/^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(networkId) && /^[0-9]*$/.test(numberOfSubs)){
+        generateSubnetIdnBroadcastId(networkId, numberOfSubs);
     }else{
         alert(`IPs not valid`);
     }
